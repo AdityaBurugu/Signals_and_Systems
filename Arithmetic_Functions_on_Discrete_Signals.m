363c48ac119dec58e2164701388e3266fb98090a
Aditya Burugu <adityaburugu1234567@gmail.com>
2023-10-11 23:26:41 +0530

    Description added

--- Arithmetic_Functions_on_Discrete_Signals.m
+++ Arithmetic_Functions_on_Discrete_Signals.m
@@ -1,60 +1,75 @@
 clear all
 close all
 
+%time range
 t=-5:1:+5;
 
+%Signal 1
 Signal_1 = (t+2).*(t>=-2 & t<=-1) + (1).*(t>-1 & t<=0)+ (2).*(t>0 & t<=1)+ (2-t).*(t>1 & t<=2);
 
-subplot(3,2,1);
+subplot(3,3,1);
 stem(t,Signal_1);
 xlabel("Time");
 ylabel("Amplitude");
 title("Signal 1");
 axis([min(t),max(t),min(Signal_1)-0.5,max(Signal_1)+0.5]);
 
-
-
+%Signal 1
 Signal_2 = (1).*(t>=-2 & t<=-1)+ (2).*(t>-1 & t<=1) + (1).*(t>1 & t<=2) + (3-t).*(t>2 & t<=3);
 
-subplot(3,2,2);
+subplot(3,3,2);
 stem(t,Signal_2);
 xlabel("Time");
 ylabel("Amplitude");
 title("Signal 2");
 axis([min(t),max(t),min(Signal_2)-0.5,max(Signal_2)+0.5]);
 
+%Signal Addition
 Signal_Addition = Signal_1 + Signal_2;
 
-subplot(3,2,3);
+subplot(3,3,3);
 stem(t,Signal_Addition);
 xlabel("Time");
 ylabel("Amplitude");
 title("Signal Addition(4D4)");
 axis([min(t),max(t),min(Signal_Addition)-0.5,max(Signal_Addition)+0.5]);
 
+%Signal Multiplication
 Signal_Multiplication = Signal_1 .* Signal_2;
 
-subplot(3,2,4);
+subplot(3,3,4);
 stem(t,Signal_Multiplication);
 xlabel("Time");
 ylabel("Amplitude");
 title("Signal Multiplication(4D4)");
 axis([min(t),max(t),min(Signal_Multiplication)-0.5,max(Signal_Multiplication)+0.5]);
 
+%Signal Subtraction
 Signal_Subtraction = Signal_1 - Signal_2;
 
-subplot(3,2,5);
+subplot(3,3,5);
 stem(t,Signal_Subtraction);
 xlabel("Time");
 ylabel("Amplitude");
 title("Signal Subtraction(4D4)");
 axis([min(t),max(t),min(Signal_Subtraction)-0.5,max(Signal_Subtraction)+0.5]);
 
+%Signal Division
 Signal_Division = Signal_1 ./ Signal_2;
 
-subplot(3,2,6);
+subplot(3,3,6);
 stem(t,Signal_Division);
 xlabel("Time");
 ylabel("Amplitude");
 title("Signal Division(4D4)");
 axis([min(t),max(t),min(Signal_Division)-0.5,max(Signal_Division)+0.5]);
+
+%Signal Exponential
+Signal_Exponential = Signal_1 .^ Signal_2;
+
+subplot(3,3,7);
+stem(t,Signal_Exponential);
+xlabel("Time");
+ylabel("Amplitude");
+title("Signal Exponential(4D4)");
+axis([min(t),max(t),min(Signal_Exponential)-0.5,max(Signal_Exponential)+0.5]);
--- Other_Functions_on_Discrete_Signals.m
+++ Other_Functions_on_Discrete_Signals.m
@@ -1,5 +1,7 @@
+%Time Range
 t=-5:1:+5;
 
+%Signal 1
 Signal_1=Signal(t);
 subplot(4,2,1);
 stem(t,Signal_1);
@@ -8,6 +10,7 @@ ylabel("Amplitude");
 title("Signal 1");
 axis([min(t),max(t),min(Signal_1)-0.5,max(Signal_1)+0.5]);
 
+%Signal Left Shifting
 Signal_Left_Shifting=Signal(t+3);
 subplot(4,2,2);
 stem(t,Signal_Left_Shifting);
@@ -16,8 +19,7 @@ ylabel("Amplitude");
 title("Signal Left Shifting(4D4)");
 axis([min(t),max(t),min(Signal_Left_Shifting)-0.5,max(Signal_Left_Shifting)+0.5]);
 
-
-
+%Signal Right Shifting
 Signal_Right_Shifting=Signal(t-3);
 subplot(4,2,3);
 stem(t,Signal_Right_Shifting);
@@ -26,6 +28,7 @@ ylabel("Amplitude");
 title("Signal Right Shifting(4D4)");
 axis([min(t),max(t),min(Signal_Right_Shifting)-0.5,max(Signal_Right_Shifting)+0.5]);
 
+%Signal Amplitude Expansion
 Signal_Amplitude_Expansion=2.*Signal(t);
 subplot(4,2,4);
 stem(t,Signal_Amplitude_Expansion);
@@ -34,6 +37,7 @@ ylabel("Amplitude");
 title("Signal Amplitude Expansion Scaling(4D4)");
 axis([min(t),max(t),min(Signal_Amplitude_Expansion)-0.5,max(Signal_Amplitude_Expansion)+0.5]);
 
+%Signal Amplitude Compression
 Signal_Amplitude_Compression=0.5.*Signal(t);
 subplot(4,2,5);
 stem(t,Signal_Amplitude_Compression);
@@ -42,6 +46,7 @@ ylabel("Amplitude");
 title("Signal Amplitude Compression Scaling(4D4)");
 axis([min(t),max(t),min(Signal_Amplitude_Compression)-0.5,max(Signal_Amplitude_Compression)+0.5]);
 
+%Signal Time Compression
 Signal_Time_Compression=Signal(2.*t);
 subplot(4,2,6);
 stem(t,Signal_Time_Compression);
@@ -50,6 +55,7 @@ ylabel("Amplitude");
 title("Signal Time Compression Scaling(4D4)");
 axis([min(t),max(t),min(Signal_Time_Compression)-0.5,max(Signal_Time_Compression)+0.5]);
 
+%Signal Time Expansion
 Signal_Time_Expansion=Signal(0.5.*t);
 subplot(4,2,7);
 stem(t,Signal_Time_Expansion);
@@ -58,6 +64,7 @@ ylabel("Amplitude");
 title("Signal Time Expansion Scaling(4D4)");
 axis([min(t),max(t),min(Signal_Time_Expansion)-0.5,max(Signal_Time_Expansion)+0.5]);
 
+%Signal Folding
 Signal_Reverse=Signal(-t);
 subplot(4,2,8);
 stem(t,Signal_Reverse);
