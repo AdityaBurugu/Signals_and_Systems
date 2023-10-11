b33da85c7654f4a1596f285d026de494f87a96ab
Aditya Burugu <adityaburugu1234567@gmail.com>
2023-10-11 23:31:50 +0530

    Power and Energy Calculation of a given signal

---
+++ Energy_Power_of_Signal.m
@@ -0,0 +1,14 @@
+%TimeStamp
+T = 10;
+
+%Time Range
+t=-T:0.01:T;
+
+Signal_1=T.*sin(2*pi.*t);
+Signal_2 = Signal_1.^2;
+
+Energy = trapz(t,Signal_2);
+Power = Energy/(2*T);
+
+disp(['Energy:E',num2str(Energy),'Joule']);
+disp(['Power:P',num2str(Power),'Joule']);
