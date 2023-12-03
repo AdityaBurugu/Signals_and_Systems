8da87b61628b6d6f606dc1233e2fb93ff4ba1ba3
Aditya Burugu <adityaburugu1234567@gmail.com>
2023-12-03 22:46:40 +0530

    Add files via upload

---
+++ Convolution_CTS.m
@@ -0,0 +1,26 @@
+clear all;
+close all;
+t1=-2:0.01:2;
+x=exp(-2.*t1).*(t1>=0) + 0.*(t1<0);
+t2=-1:0.01:3;
+h=1.*(t2>=0) + 0.*(t2<0);
+y=conv(x,h);
+a=min(t1)+min(t2);
+b=max(t1)+max(t2);
+t3=a:0.01:b;
+
+subplot(3,1,1);
+plot(t1,x);
+xlabel("time t");
+ylabel("amplitude x");
+title("input signal");
+subplot(3,1,2);
+plot(t2,h);
+xlabel("time t");
+ylabel("amplitude x");
+title("system response");
+subplot(3,1,3);
+plot(t3,y)
+xlabel("time t");
+ylabel("amplitude x");
+title("convloution");
---
+++ Convolution_DTS.m
@@ -0,0 +1,26 @@
+clear all;
+close all;
+n1=-1:1:2;
+x=[1 2 3 -1];
+n2=0:1:4;
+h=[2 -1 -2 4 3];
+y=conv(x,h);
+a=min(n1)+min(n2);
+b=max(n1)+max(n2);
+n3=a:1:b;
+
+subplot(3,1,1);
+stem(n1,x);
+xlabel("time t");
+ylabel("amplitude x");
+title("input signal");
+subplot(3,1,2);
+stem(n2,h);
+xlabel("time t");
+ylabel("amplitude x");
+title("system response");
+subplot(3,1,3);
+stem(n3,y)
+xlabel("time t");
+ylabel("amplitude x");
+title("convloution");
